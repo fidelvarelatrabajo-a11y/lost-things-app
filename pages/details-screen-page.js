41d001e546054a3629b935f862ac8fe874cdedeb
fidel.varela.trabajo@gmail.com <fidel.varela.trabajo@gmail.com>
2025-10-03 18:16:30 -0600

    feat: creacion de pagina listado de reportes

---
+++ pages/details-screen-page.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
+import { styles } from "../styles/styles";
+import { db } from "../firebase";
+
+export function DetailsScreenPage() {
+  const [reportes, setReportes] = useState([]);
+
+  useEffect(() => {
+    // Creamos una consulta: ordenar por fecha descendente
+    const q = query(collection(db, "reportes"), orderBy("createdAt", "desc"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const lista = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setReportes(lista);
+    });
+
+    return () => unsubscribe();
+  }, []);
+  console.log('Reportes',reportes);
+  console.log('Set Reportes', reportes);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>📋 Reportes </Text>
+      <FlatList
+        data={reportes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.textoTitulo}>Objeto Extraviado</Text>
+            <Text>{item.description}</Text>
+            <TouchableOpacity style = {styles.buttonDetails} onPress={()=>{console.log('Hola amigoo')}}>
+                <Text style={styles.buttonText}> Ver detalles </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
