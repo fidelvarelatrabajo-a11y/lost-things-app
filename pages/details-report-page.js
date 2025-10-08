6b9c579b1a3b18937383441a3ceb53a5e1fee64c
fidel.varela.trabajo@gmail.com <fidel.varela.trabajo@gmail.com>
2025-10-08 17:08:43 -0600

    feat: se agrego la pagina detalle de reporte

---
+++ pages/details-report-page.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, Image, Alert } from 'react-native';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { styles } from '../styles/styles'; 
+export function DetailScreen({ route }) {
+  const { id } = route.params;
+  const [item, setItem] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDetail = async () => {
+      try {
+        const docRef = doc(db, "reportes", id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setItem(docSnap.data());
+        } else {
+          console.log("No se encontró el documento");
+          Alert.alert('Upss','No se pudo recuperar el detalle')
+        }
+      } catch (error) {
+        console.error("Error al obtener detalle:", error);
+        Alert.alert('Error','Ocurrio un error al tratar de obtener el detalle')
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDetail();
+  }, [id]);
+
+  if (loading) return <ActivityIndicator size="large" color="#000" />;
+  if (!item) return <Text>No se encontró el artículo</Text>;
+  console.log('*****esta es la info*****',item)
+  return (
+    <View style={ styles.container }>
+      <Text style={styles.title}>Producto Extraviado</Text>
+      <Text style = {styles.textDescription}>{item.description}</Text>
+      <Image source={{ uri: item.imageUrl }} style={styles.image} />
+    </View>
+  );
+}
